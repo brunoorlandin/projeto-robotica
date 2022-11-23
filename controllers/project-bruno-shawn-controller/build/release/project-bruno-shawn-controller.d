50899b346e519036ab85b24f47ea27777356ddbe
brunoorlandin <bruno.orlandin@hotmail.com>
2022-11-23 18:48:26 -0300

    inicial webots setup

---
+++ controllers/project-bruno-shawn-controller/build/release/project-bruno-shawn-controller.d
@@ -0,0 +1,7 @@
+build/release/project-bruno-shawn-controller.o: \
+ project-bruno-shawn-controller.c \
+ F:\Webots/include/controller/c/webots/robot.h \
+ F:\Webots/include/controller/c/webots/types.h \
+ F:\Webots/include/controller/c/webots/nodes.h \
+ F:\Webots/include/controller/c/webots/distance_sensor.h \
+ F:\Webots/include/controller/c/webots/motor.h
