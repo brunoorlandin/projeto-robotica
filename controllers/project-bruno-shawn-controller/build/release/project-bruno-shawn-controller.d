bd87aca0d595d7a52fe96fa194d45c6499c4e41a
Shawn Kawabe <59843897+shawnkawabe@users.noreply.github.com>
2022-11-23 22:29:57 -0300

    add changes clock 100

--- controllers/project-bruno-shawn-controller/build/release/project-bruno-shawn-controller.d
+++ controllers/project-bruno-shawn-controller/build/release/project-bruno-shawn-controller.d
@@ -1,7 +1,7 @@
 build/release/project-bruno-shawn-controller.o: \
  project-bruno-shawn-controller.c \
- F:\Webots/include/controller/c/webots/robot.h \
- F:\Webots/include/controller/c/webots/types.h \
- F:\Webots/include/controller/c/webots/nodes.h \
- F:\Webots/include/controller/c/webots/distance_sensor.h \
- F:\Webots/include/controller/c/webots/motor.h
+ C:\Users\shawn\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\shawn\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\shawn\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\shawn\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Users\shawn\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h
--- controllers/project-bruno-shawn-controller/project-bruno-shawn-controller.c
+++ controllers/project-bruno-shawn-controller/project-bruno-shawn-controller.c
@@ -1,19 +1,14 @@
 #include <webots/robot.h>
 #include <webots/distance_sensor.h>
 #include <webots/motor.h>
+#include <stdio.h>
 
-// time in [ms] of a simulation step
-#define TIME_STEP 64
-
+#define TIME_STEP 100
 #define MAX_SPEED 6.28
 
-// entry point of the controller
 int main(int argc, char **argv) {
-
-  // initialize the Webots API
   wb_robot_init();
 
-  // internal variables
   int i;
   WbDeviceTag ps[8];
   char ps_names[8][4] = {
@@ -21,7 +16,6 @@ int main(int argc, char **argv) {
     "ps4", "ps5", "ps6", "ps7"
   };
 
-  // initialize devices
   for (i = 0; i < 8 ; i++) {
     ps[i] = wb_robot_get_device(ps_names[i]);
     wb_distance_sensor_enable(ps[i], TIME_STEP);
@@ -31,48 +25,44 @@ int main(int argc, char **argv) {
   WbDeviceTag right_motor = wb_robot_get_device("right wheel motor");
   wb_motor_set_position(left_motor, INFINITY);
   wb_motor_set_position(right_motor, INFINITY);
-  wb_motor_set_velocity(left_motor, 0.0);
-  wb_motor_set_velocity(right_motor, 0.0);
-
-  // feedback loop: step simulation until an exit event is received
+  wb_motor_set_velocity(left_motor, 6.0);
+  wb_motor_set_velocity(right_motor, 6.0);
+  
+  double left_speed;
+  double right_speed;
   while (wb_robot_step(TIME_STEP) != -1) {
-    // read sensors outputs
     double ps_values[8];
     for (i = 0; i < 8 ; i++)
       ps_values[i] = wb_distance_sensor_get_value(ps[i]);
 
-    // detect obstacles
-    bool right_obstacle =
-      ps_values[0] > 80.0 ||
-      ps_values[1] > 80.0 ||
-      ps_values[2] > 80.0;
-    bool left_obstacle =
-      ps_values[5] > 80.0 ||
-      ps_values[6] > 80.0 ||
-      ps_values[7] > 80.0;
 
-    // initialize motor speeds at 50% of MAX_SPEED.
-    double left_speed  = 0.1 * MAX_SPEED;
-    double right_speed = 0.1 * MAX_SPEED;
-
-    // modify speeds according to obstacles
-    if (left_obstacle) {
-      // turn right
-      left_speed  = 0.5 * MAX_SPEED;
-      right_speed = -0.5 * MAX_SPEED;
+    bool left_obstacle =    ps_values[5] > 80.0;
+    bool left_sensor =      ps_values[6] > 80.0;
+    bool front_obstacle =   ps_values[7] > 80.0;
+    if(front_obstacle){
+      printf("1");
+      left_speed  = 1 * MAX_SPEED;
+      right_speed = -1 * MAX_SPEED;
+    } else {
+      if(left_obstacle){
+        printf("2");
+        left_speed  = 1 * MAX_SPEED;
+        right_speed = 1 * MAX_SPEED;
+      } else {
+        printf("3");
+        left_speed  = 1 * MAX_SPEED/8;
+        right_speed = 1 * MAX_SPEED;
+      }
+      if(left_sensor){
+        printf("4");
+        left_speed  = 1 * MAX_SPEED;
+        right_speed = 1 * MAX_SPEED/8;
+      }
     }
-    else if (right_obstacle) {
-      // turn left
-      left_speed  = -0.5 * MAX_SPEED;
-      right_speed = 0.5 * MAX_SPEED;
-    }
-
-    // write actuators inputs
     wb_motor_set_velocity(left_motor, left_speed);
     wb_motor_set_velocity(right_motor, right_speed);
   }
 
-  // cleanup the Webots API
   wb_robot_cleanup();
-  return 0; //EXIT_SUCCESS
+  return 0; 
 }
